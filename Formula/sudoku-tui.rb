2dbf2e887454e6320c40ff435ae4a0dca46c23d2
Daljeet Sandu <daljeet.sandu@datadoghq.com>
2026-02-22 11:53:46 +0100

    Update to v0.2.1

--- Formula/sudoku-tui.rb
+++ Formula/sudoku-tui.rb
@@ -1,26 +1,26 @@
 class SudokuTui < Formula
   desc "Terminal-based Sudoku game with competitive multiplayer"
   homepage "https://github.com/Dalje-et/sudoku-tui"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.0/sudoku-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "691b0c1be541c3fea55818bf75f5d01efc8a4ed3f3b655ea056443ecda62357c"
+      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.1/sudoku-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "33c8f6519828051449511c2d58f1d402f60fd45d067957c123c2ef0cc94212f4"
     else
-      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.0/sudoku-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "efb8e5102265a101b5aa02e866ace0451aead811606c9c52c98a554ae5cec9ee"
+      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.1/sudoku-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "23c2cb58d1deaac078795250edb4c91d713b01dcb099438ae44a2227a76e6692"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.0/sudoku-tui-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "63cdb9eaadade60f7733c980cedfd3a8d1e58fe72840815cb175eba85f4ee62f"
+      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.1/sudoku-tui-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "44dd2e5a5602cb383a39c9ffb0bf6556505947c087e058e1909741ea5e971446"
     else
-      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.0/sudoku-tui-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9d7f06d92ceeab08f2903df33b90ab84434081d42e7a1a89ec8df9dced9b283d"
+      url "https://github.com/Dalje-et/sudoku-tui/releases/download/v0.2.1/sudoku-tui-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "062ad04808ba5d5980336760023c05ba3a7752a79ace8408723459cf1d5de32e"
     end
   end
 
